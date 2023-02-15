0a22684867c501ef71ac94b3c86494602691fc8f
Chase <chase.anderson.000@gmail.com>
2023-02-15 01:37:42 -0600

    almost working HW2

--- biomedical_optics_hw/monte_HW2.py
+++ biomedical_optics_hw/monte_HW2.py
@@ -1,35 +1,51 @@
 # Chase Anderson
-# Biomedical Optics HW1
-# monte.py
+# Biomedical Optics HW2
+# monte_HW2.py
 
 import random
 import numpy as np
 import matplotlib.pyplot as plt                     
 
-def new_deflection():
+def new_deflection(gee):
     
-    if(g==0):
-        theta = np.arccos(2*random.uniform(0,1)-1)
+    if(gee==0):
+        t = np.arccos(2*random.uniform(0,1)-1)
     else:
-        theta = np.arccos((1 + g**2 - ((1-g**2)/(1-g+2*g*random.uniform(0,1)))**2)/(2*g))
+        t = np.arccos((1 + gee**2 - ((1-gee**2)/(1-gee+2*gee*random.uniform(0,1)))**2)/(2*g))
     
-    return theta
+    return t
 
-def new_trajectory():
+def new_trajectory(t, p, mux, muy, muz):
     
-    if(np.abs(mu_z) > 0.99999):
-        mu_x_new = np.sin(theta)*np.cos(phi)
-        mu_y_new = np.sin(theta)*np.sin(phi)
-        mu_z_new = mu_z * np.cos(theta)/np.abs(mu_z)
+    if(np.abs(muz) > 0.99999):
+        mu_x_new = np.sin(t)*np.cos(p)
+        mu_y_new = np.sin(t)*np.sin(p)
+        mu_z_new = muz * np.cos(t)/np.abs(muz)
     else:
-        mu_x_new = np.sin(theta)/np.sqrt(1-mu_z*mu_z)*(mu_x*mu_z*np.cos(phi)-mu_y*np.sin(phi))+mu_x*np.cos(theta)
-        mu_y_new = np.sin(theta)/np.sqrt(1-mu_z*mu_z)*(mu_y*mu_z*np.cos(phi)+mu_x*np.sin(phi))+mu_y*np.cos(theta)
-        mu_z_new = -np.sin(theta)*np.cos(phi)*np.sqrt(1-mu_z*mu_z)+mu_z*np.cos(theta)
+        mu_x_new = np.sin(t)/np.sqrt(1-muz*muz)*(mux*muz*np.cos(p)-muy*np.sin(p))+mux*np.cos(t)
+        mu_y_new = np.sin(t)/np.sqrt(1-muz*muz)*(muy*muz*np.cos(p)+mux*np.sin(p))+muy*np.cos(t)
+        mu_z_new = -np.sin(t)*np.cos(p)*np.sqrt(1-muz*muz)+muz*np.cos(t)
         
     return mu_x_new,mu_y_new,mu_z_new
 
 def monte_carlo_simulation():
 
+    a = 0
+    r = 0
+    t = 0
+    
+    n = 100000 # photons
+
+    mu_a = 15 # inverse cm
+    mu_s = 85 # inverse cm
+
+    theta = 0
+    phi = 0
+
+    s = 0
+    g = 0
+    d = 0.03 # cm
+    
     for j in range(n):
         
         loop = True
@@ -51,55 +67,33 @@ def monte_carlo_simulation():
             z = z + mu_z*s
             
             if(z >= d):
-                transmit = transmit + 1
+                t = t + 1
                 loop = False
             elif(z <= 0):
-                reflect = reflect + 1
+                r = r + 1
                 loop = False
-            elif(random.uniform(0, 1) <= ((mu_a)*(mu_z*s))):
-                absorb = absorb + 1
+            elif(random.uniform(0, 1) <= ((mu_a)*s)):
+                a = a + 1
                 loop = False
             else:
-                new_deflection()
-                new_trajectory()
-
-n = 100000 # photons
-
-mu_s = 85 # inverse cm
-mu_a = 15 # inverse cm
-
-loop = True
-
-x = 0
-y = 0
-z = 0
-
-mu_x = 0
-mu_y = 0
-mu_z = 1
-
-mu_x_new = 0
-mu_y_new = 0
-mu_z_new = 0
-
-theta = 0
-phi = 0
-
-s = 0
-g = 0
-d = 0.03 # cm
+                theta = new_deflection(g)
+                mu_x, mu_y, mu_z = new_trajectory(theta, phi, mu_x, mu_y, mu_z)
+    
+    return a, r, t
 
 absorb = 0
 reflect = 0
 transmit = 0
 
-for i in range(5):
+for i in range(1):
     
     absorb = 0
     reflect = 0
     transmit = 0
     
-    monte_carlo_simulation()
+    absorb, reflect, transmit = monte_carlo_simulation()
+    
+    print("absorb: " + str(absorb) + " | reflect: " + str(reflect) + " | transmit: " + str(transmit) + " | TOTAL: " + str(absorb+reflect+transmit));
     
     labels = ['ABSORBED','REFLECTED','TRANSMITTED']
     sizes = [absorb, reflect, transmit]
